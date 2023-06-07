f6d3a8e83e4e2dd9e262a8cce0ad9d3e3a4d5452
ciamew <113471943+ciamew@users.noreply.github.com>
2023-06-07 18:17:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import random
 
 win = tk.Tk()
 
@@ -9,38 +8,41 @@ h = 500
 head = [500//2,500//2]
 moving = [0,-1]
 whole_snake = []
-speed = 100
+speed = 100 #og speed
+snake_coordinates = []
 
-canvas = tk.Canvas(width = w, height = h, bg = "white")
+canvas = tk.Canvas(width = w, height = h, bg = "black")
 canvas.pack()
-
 
 def draw_snake():
     global head
-    canvas.create_rectangle(head[0],head[1],head[0]+1,head[1]+1,outline="red",fill="black",width=10)
+    canvas.create_rectangle(head[0],head[1],head[0]+1,head[1]+1,outline="white",fill="black",width=3)
+    snake_coordinates.append(tuple(head)) #vopcha suradnice hada do snake_coordinates
+    if snake_coordinates.count(tuple(head)) > 1: #ak sa dana suradnica vyskitne viac ako 1x konci hra
+        canvas.delete("all")
+        canvas.create_text(w//2, 400, text="YOU LOST!", font="Arial 15", fill="red")
+        return
     head[0] += moving[0]
     head[1] += moving[1]
     canvas.after(speed,draw_snake)
-
+    print(snake_coordinates)
 draw_snake()
 
 def changer(e):
-    global moving
-    global speed
+    global moving, speed
     print("clicked")
-    if e.char=="w":
+    if e.char == "w": #up
         moving = [0,-1]
-    elif e.char == "a":
+    elif e.char == "a": #left
         moving = [-1,0]
-    elif e.char == "d":
+    elif e.char == "d": #right
         moving = [1,0]
-    elif e.char == "s":
+    elif e.char == "s": #down
         moving = [0,1]
-    elif e.char == "k":
+    elif e.char == "k": #makes the snake go faster
         speed -= 10
-    elif e.char == "l":
+    elif e.char == "l": #makes the snake go slower
         speed += 10
-
 
 win.bind("<KeyPress>",changer)
 
